adf69b3199d5348dc09c266cbd9cf914d35dc6ba
yuta <az7xj5xn@yahoo.co.jp>
2021-03-04 16:13:27 +0900

    Postモデル作った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_095659) do
+ActiveRecord::Schema.define(version: 2021_03_04_071151) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
