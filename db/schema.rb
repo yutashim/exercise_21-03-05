dfb2a4d3b831c6cfd22bf11909436480b494ae10
yuta <az7xj5xn@yahoo.co.jp>
2021-03-04 16:15:16 +0900

    Labelモデルつくった

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_071151) do
+ActiveRecord::Schema.define(version: 2021_03_04_071359) do
+
+  create_table "labels", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
