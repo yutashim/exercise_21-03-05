830124a72f62fc93e0e7b26db910c663b7448aa2
yuta <az7xj5xn@yahoo.co.jp>
2021-03-05 17:25:32 +0900

    LetterOpenerの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.developtment?
 end
