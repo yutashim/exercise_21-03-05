122d1076993ac9f873399aa4147fbee5d37308ac
yuta <az7xj5xn@yahoo.co.jp>
2021-03-05 15:12:39 +0900

    ラベル追加の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,34 +18,25 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
-    @labels = Label.where(publicness: true).or(Label.where(id: @post.label_ids))
+    @labels = Label.where(publicness: true).or(Label.where(creator_id: @post.id))
   end
 
   # POST /posts or /posts.json
   def create
-    raise
     @post = Post.new(post_params)
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      new_label(@post)
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.update(post_params)
+      new_label(@post)
+    else
+      render :edit
     end
   end
 
@@ -67,5 +58,18 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:content, label_ids: [])
+    end
+
+    def new_label(post)
+      @label = post.private_labels.build(content: params[:post][:new_label]) if params[:post][:new_label].present?
+      if !@label
+        redirect_to post_path(post)
+      elsif @label.save
+        post.label_ids = post.label_ids << @label.id
+        redirect_to post_path(post)
+      else
+        redirect_to edit_post_path(post)
+        flash[:notice] = @label.errors.full_messages
+      end
     end
 end
