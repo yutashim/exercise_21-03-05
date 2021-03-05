5b9ca160a6ea161dbee73e53de7903a3ba782984
yuta <az7xj5xn@yahoo.co.jp>
2021-03-05 15:16:45 +0900

    private_labelsにdependent設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   has_many :labelings, dependent: :destroy
   has_many :labels, through: :labelings
-  has_many :private_labels, class_name: 'Label', foreign_key: 'creator_id'
+  has_many :private_labels, class_name: 'Label', foreign_key: 'creator_id', dependent: :destroy
 end
