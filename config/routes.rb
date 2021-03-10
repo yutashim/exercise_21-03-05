ac394ec3c9554d7e43ca814a747e673c10ce2eff
yuta <az7xj5xn@yahoo.co.jp>
2021-03-10 22:06:13 +0900

    DeviseUserの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :devise_users
+  root 'posts#index'
   resources :posts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
